570ae10785c2e69587022bb9f7dbfe3c66e96fb0
BhatiPankaj <4121998bhati@gmail.com>
2020-10-26 12:53:09 +0530

    test case pass to handle number is nmultiple of 3 and 5

--- src/play/FizzBuzz.java
+++ src/play/FizzBuzz.java
@@ -3,7 +3,10 @@ package play;
 public class FizzBuzz {
 	public String fizzBuzzPlay(int number){
 		
-		if(number%3 == 0) {
+		if(number%5 == 0 && number%3 == 0) {
+			return "FizzBuzz";
+		}
+		else if(number%3 == 0) {
 			return "Fizz";
 		}
 		else if(number%5 == 0) {
