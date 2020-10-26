43c1ca20b5add08e7c471b0bcbed633588dc292d
BhatiPankaj <4121998bhati@gmail.com>
2020-10-26 12:40:47 +0530

    test case fail to handle number is multiple of 3

--- src/test/FizzBuzzTest.java
+++ src/test/FizzBuzzTest.java
@@ -8,6 +8,7 @@ import play.FizzBuzz;
 
 public class FizzBuzzTest {
 	private FizzBuzz fizzy;
+	private String fizz = "Fizz";
 	
 	@Before
 	public void setup() {
@@ -21,5 +22,11 @@ public class FizzBuzzTest {
 		Assert.assertEquals("7", fizzy.fizzBuzzPlay(7));
 		Assert.assertEquals("8", fizzy.fizzBuzzPlay(8));
 		Assert.assertEquals("13", fizzy.fizzBuzzPlay(13));
+	}
+	
+	@Test
+	public void numberIsMultipleOf_3() {
+		Assert.assertEquals(fizz, fizzy.fizzBuzzPlay(3));
+		Assert.assertEquals(fizz, fizzy.fizzBuzzPlay(6));
 	}
 }
